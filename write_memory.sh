00ca4b0a7258faa9f0afde3a9dac5a564ee34c96
Bryan Pearson <peabryan95@gmail.com>
2022-04-21 13:38:36 -0400

    Update write_memory

--- write_memory.sh
+++ write_memory.sh
@@ -4,8 +4,8 @@
 
 command_prefix="mosquitto_pub -h 0.0.0.0 -t /topic/qos0 -m"
 
-# The 3 most significant bytes of the supplied base address
-msb=$(echo -n $1 | tail -6c)
+msb=$(echo -n $1 | tail -6c) # 3 most significant bytes
+lsb=$(echo $1 | head -2c) # least significant bytes
 
 write_r="%110x%6\$hhn" # 0x72 - 4 = 0x6e
 write_o="%107x%6\$hhn" # 0x6f - 4 = 0x6b
@@ -16,7 +16,6 @@ write_null="%252x%6\$hhn" # 0x00 - 4 = 0xfc
 for i in {0..4}
 do
     # Add $i to the least significant byte
-    lsb=$(echo $1 | head -2c)
     lsb_decimal=$(printf "%d" $((16#$lsb+$i)))
     lsb_hex=$(printf "%.2x" $lsb_decimal)
 
